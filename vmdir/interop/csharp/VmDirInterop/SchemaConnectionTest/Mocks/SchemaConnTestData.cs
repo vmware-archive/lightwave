378d4ed55ad0fa701cd982c8de989268ad814882
sruo <sruo@vmware.com>
2016-07-13 13:26:12 -0700

    sync stable/vmdir/interop/... up to 4285169 (csharp for UI)

---
+++ vmdir/interop/csharp/VmDirInterop/SchemaConnectionTest/Mocks/SchemaConnTestData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VmDirInterop.Schema;
+using VmDirInterop.Schema.Entries;
+
+namespace SchemaConnectionTest.Mocks
+{
+    public class SchemaConnTestData
+    {
+        public String HostName { get; set; }
+        public Boolean Reachable { get; set; }
+        public DseRootEntry DseRootEntry { get; set; }
+        public IDictionary<String, ServerEntry> ServerEntries { get; set; }
+        public SubSchemaSubEntry SubSchemaSubEntry { get; set; }
+        public SchemaComparableList<SchemaEntry> AttributeSchemaEntries { get; set; }
+        public SchemaComparableList<SchemaEntry> ClassSchemaEntries { get; set; }
+    }
+}
---
+++ vmdir/interop/csharp/VmDirInterop/VmDirInterop/Schema/Utils/Tuple.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Copyright © 2012-2015 VMware, Inc.  All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the “License”); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *·
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an “AS IS” BASIS, without
+ * warranties or conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * NOTE:
+ * This is manual implementation of Tuple class.
+ * Remove this when interop is upgraded to .Net Framework 4.0 or above.
+ */
+namespace VmDirInterop.Schema.Utils
+{
+    public class Tuple<T1, T2>
+    {
+        public T1 item1 { get; private set; }
+        public T2 item2 { get; private set; }
+        public Tuple(T1 t1, T2 t2)
+        {
+            item1 = t1;
+            item2 = t2;
+        }
+    }
+}
