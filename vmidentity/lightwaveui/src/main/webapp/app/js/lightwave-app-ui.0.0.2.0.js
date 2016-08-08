dcee205d28ceb473a58b7956bac2954aba6132f9
Nimish Bhonsale <nbhonsale@vmware.com>
2016-08-08 09:49:05 +0530

    Bug fix-1704222, reduced transparency for dialog, 1705143 Extended Lightwave UI to support Microsoft Edge 25, EdgeHTML 13.1 browser
    Change-Id: Ib39da405d43048529f05c7f5cdea025530178b08

--- vmidentity/lightwaveui/src/main/webapp/app/js/lightwave-app-ui.0.0.2.0.js
+++ vmidentity/lightwaveui/src/main/webapp/app/js/lightwave-app-ui.0.0.2.0.js
@@ -58,3 +58,14 @@ function run($rootScope, $location, $cookieStore, $window) {
         }
     });
 }
+
+app.config(['$httpProvider', function($httpProvider) {
+    if (!$httpProvider.defaults.headers.get) {
+        $httpProvider.defaults.headers.get = {};
+    }
+
+    //disable IE ajax request caching
+    $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
+    $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+    $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
+}]);
