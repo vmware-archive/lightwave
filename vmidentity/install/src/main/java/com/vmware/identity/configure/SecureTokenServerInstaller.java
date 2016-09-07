1a83cff1900973b76f42ea069084ec41a60015b7
Prashant shukla <prashantshukla@vmware.com>
2016-09-07 16:56:38 +0000

    Add support for container upgrade
    Change-Id: I8c93515cd37f0458bae70475e7f580fbe966e74a

--- vmidentity/install/src/main/java/com/vmware/identity/configure/SecureTokenServerInstaller.java
+++ vmidentity/install/src/main/java/com/vmware/identity/configure/SecureTokenServerInstaller.java
@@ -89,7 +89,7 @@ public class SecureTokenServerInstaller implements IPlatformComponentInstaller {
         log.info("Configuring STS");
         configureSTS();
 
-        String tcSTSBase = "";
+        String tcSTSBase = InstallerUtils.getInstallerHelper().getTCBase(); 
         Path tomcatTempDir = Paths.get(tcSTSBase, "temp");
         if (!Files.exists(tomcatTempDir)) {
             Files.createDirectories(tomcatTempDir);
