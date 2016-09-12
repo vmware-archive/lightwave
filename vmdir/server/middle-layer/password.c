beca0b835f2646269084938fad39db12714ecd3d
sruo <sruo@vmware.com>
2016-09-12 15:54:27 -0700

    port PR 1725446,1725051 for VMIT

--- vmdir/server/middle-layer/password.c
+++ vmdir/server/middle-layer/password.c
@@ -989,24 +989,28 @@ PasswdModifyMetaDataCreate(
 
         pOldPasswdAttr = VmDirFindAttrByName(pEntry, ATTR_OLD_USER_PASSWORD);
 
-        // TODO, could consider cache this in Operation if multiple lookup needed
         dwError = VdirGetPasswdAndLockoutPolicy(
                         BERVAL_NORM_VAL(pOperation->request.modifyReq.dn),
                         &policy);
-        // ignore error - if no policy, use default value PASSWD_DEFAULT_RETENTION_COUNT.
+        if (dwError == 0 && policy.bEnabled && policy.iRecycleCnt > 0)
+        {   // if we have an enabled policy, do recycle related task/check
+            dwError = OldPasswdRetention(
+                            pOperation,
+                            pOldPasswdAttr ? (&(pOldPasswdAttr->vals[0])) : NULL, // current retended blob
+                            pCurrentPassValue,              // current (before modify) passwd blob
+                            policy.iRecycleCnt,
+                            &bvRetendedBlob);               // new retended blob
+            BAIL_ON_VMDIR_ERROR_WITH_MSG(dwError, pszLocalErrMsg, "Old password save failed");
 
-        dwError = OldPasswdRetention(
-                        pOperation,
-                        pOldPasswdAttr ? (&(pOldPasswdAttr->vals[0])) : NULL, // current retended blob
-                        pCurrentPassValue,              // current (befoer modify) passwd blob
-                        policy.iRecycleCnt ? policy.iRecycleCnt : PASSWD_DEFAULT_RETENTION_COUNT,
-                        &bvRetendedBlob);               // new retended blob
-        BAIL_ON_VMDIR_ERROR_WITH_MSG(dwError, pszLocalErrMsg, "Old password save failed");
-
-        dwError = OldPasswdRecycleCheck(
-                        pszNewClearPasswd,
-                        &bvRetendedBlob);
-        BAIL_ON_VMDIR_ERROR_WITH_MSG(dwError, pszLocalErrMsg, "Password recycle");
+            dwError = OldPasswdRecycleCheck(
+                            pszNewClearPasswd,
+                            &bvRetendedBlob);
+            BAIL_ON_VMDIR_ERROR_WITH_MSG(dwError, pszLocalErrMsg, "Password recycle");
+        }
+        else if (dwError)
+        {
+            dwError = 0; // ignore policy lookup error
+        }
     }
 
 cleanup:
