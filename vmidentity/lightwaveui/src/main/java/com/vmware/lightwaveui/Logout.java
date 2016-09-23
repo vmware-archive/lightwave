6761cf9c206202c9c5e5c9182b34e5e937930729
Nimish Bhonsale <nbhonsale@vmware.com>
2016-09-23 13:45:50 +0530

    Bug fix: 1727231

--- vmidentity/lightwaveui/src/main/java/com/vmware/lightwaveui/Logout.java
+++ vmidentity/lightwaveui/src/main/java/com/vmware/lightwaveui/Logout.java
@@ -59,8 +59,8 @@ public class Logout extends HttpServlet {
 		}
 		
 		String uri = request.getRequestURL().toString();
-		String server = uri.split("://")[1].split("/")[0].split(":")[0];
-		//String server = new ServerHelper().getHostname();
+		//String server = uri.split("://")[1].split("/")[0].split(":")[0];
+		String server = new ServerHelper().getHostname();
 		String postLogoutRedirectUri = "https://" + server + "/lightwaveui";
 		String openIdConnectUri = "https://" + server + "/openidconnect/logout/" + value_tenant;
 		String args = "?id_token_hint=" + value_id_token +
