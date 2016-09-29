83907ebc92594e2ae55a1a550fdec9fe14f45ff9
Nimish Bhonsale <nbhonsale@vmware.com>
2016-09-30 03:21:28 +0530

    PR-1736206 Fix copyright notice to 2016 in UI

--- tools/win/VMRestSsoAdminSnapIn/RestSsoAdminSnapIn/Presenters/Nodes/ServerNode.cs
+++ tools/win/VMRestSsoAdminSnapIn/RestSsoAdminSnapIn/Presenters/Nodes/ServerNode.cs
@@ -11,6 +11,7 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
+
 using System;
 using System.Linq;
 using Vmware.Tools.RestSsoAdminSnapIn.Dto;
