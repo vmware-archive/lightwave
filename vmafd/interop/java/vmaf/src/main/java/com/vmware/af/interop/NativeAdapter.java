8038edaed0cb1bc1baa4be51de5fa75f37ddf904
Aishu Raghavan <araghavan@vmware.com>
2017-03-01 15:08:13 -0800

    More java changes to avoid hard coded path
    Change-Id: Id9713d84323a5eb5bacae9c6f56a615148c41134

--- vmafd/interop/java/vmaf/src/main/java/com/vmware/af/interop/NativeAdapter.java
+++ vmafd/interop/java/vmaf/src/main/java/com/vmware/af/interop/NativeAdapter.java
@@ -46,6 +46,7 @@ public abstract class NativeAdapter
         final String propName = "jna.library.path";
 
         final String LINUX_VMDIR_LIB64_PATH    = "/opt/vmware/lib64";
+        final String LINUX_VMDIR_VC_LIB64_PATH    = "/usr/lib/vmware-vmafd/lib64";
         final String LINUX_LIKEWISE_LIB64_PATH = "/opt/likewise/lib64";
 
         final String WIN_REG_VMDIR_PATH =
@@ -67,7 +68,8 @@ public abstract class NativeAdapter
         {
             paths = Arrays.asList(
                         LINUX_VMDIR_LIB64_PATH,
-                        LINUX_LIKEWISE_LIB64_PATH);
+                        LINUX_LIKEWISE_LIB64_PATH,
+                        LINUX_VMDIR_VC_LIB64_PATH);
         }
         else if (SystemUtils.IS_OS_WINDOWS)
         {
@@ -101,14 +103,14 @@ public abstract class NativeAdapter
         }
 
         // Check if the paths exist
-        for (String pathString : paths)
-        {
-            Path path = Paths.get(pathString);
-            if (Files.notExists(path))
-            {
-                throw new IllegalStateException("Path \"" + pathString + "\" does not exist");
-            }
-        }
+        //for (String pathString : paths)
+        //{
+            //Path path = Paths.get(pathString);
+            //if (Files.notExists(path))
+            //{
+                //throw new IllegalStateException("Path \"" + pathString + "\" does not exist");
+            //}
+        //}
 
         String propValue = System.getProperty(propName);
 
