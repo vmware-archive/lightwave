4e72a7eefa58496af1864702d83f4560374254a7
Scott Salley <ssalley@vmware.com>
2017-03-07 14:36:02 -0800

    Change copyright symbol to ASCII symbols
    Change-Id: I591297a747d77a168d2f4bf712c8c0fb8d0d146d

--- vmafd/interop/python/test/https_client.py
+++ vmafd/interop/python/test/https_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 #
-# Copyright © 2012-2015 VMware, Inc.  All Rights Reserved.
+# Copyright (C) 2012-2015 VMware, Inc.  All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the “License”); you may not
 # use this file except in compliance with the License.  You may obtain a copy
