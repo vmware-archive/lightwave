cc4b001afd0b955d110c74fdc31df2b1ad2752e6
Jonathan Brown <brownj@vmware.com>
2017-04-24 15:04:10 -0700

    Updated top-level configure.ac and Makefile.am
    Change-Id: Iec9a53d7bf6314759551bf664abe8a2068a7074f

--- build.sh
+++ build.sh
@@ -1,36 +1,20 @@
 #!/bin/sh
+
+autoreconf -vif .
+
 ./configure \
-     CFLAGS="-Wall -Werror -Wno-unused-but-set-variable -Wno-pointer-sign -Wimplicit-function-declaration -Wno-address -Wno-enum-compare" \
+     CFLAGS="-Wall -Werror -Wno-unused-but-set-variable -Wno-pointer-sign -Wno-implicit-function-declaration -Wno-address -Wno-enum-compare" \
     --prefix=/opt/vmware \
     --libdir=/opt/vmware/lib64 \
-    --localstatedir=/var/lib/vmware/vmdir \
-    --with-java=/var/ZZ
     --with-vmevent=/home/brownj/workspaces/git/pslabs/lightwave/stage/opt/vmware \
+    --with-vmdir=/home/brownj/workspaces/git/pslabs/lightwave/stage/opt/vmware \
+    --with-vmafd=/home/brownj/workspaces/git/pslabs/lightwave/stage/opt/vmware \
+    --with-vmdns=/home/brownj/workspaces/git/pslabs/lightwave/stage/opt/vmware \
     --with-likewise=/opt/likewise \
-    --with-config=./config \
     --with-ssl=/usr \
+    --with-jansson=/usr \
+    --with-java=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.121-10.b14.fc25.x86_64 \
+    --with-ant=/usr/share/ant \
+    --with-config=./config \
     --with-version="4.4.4" \
     --enable-lightwave-build=yes
-
-
-#
-#    --with-vmevent=./vmevent \
-#    --with-vmdir=./vmdir \
-#    --with-vmafd=./vmafd \
-#    --with-vmdns=./vmdns \
-#
-# vmevent options
-#
-# vmdir options
-#    --enable-server=yes
-#    --with-datastore=mdb
-#    --with-sasl=/usr
-#    --with-logdir=/var/log
-#
-#
-# vmafd options
-#
-# vmca options
-#
-# vmdns options
-#
