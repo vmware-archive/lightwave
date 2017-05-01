ac2691216d6dd8f3c4b37263a92779e342648dfb
Aishu Raghavan <araghavan@vmware.com>
2017-05-01 14:47:36 -0700

    Ubuntu_build
    Change-Id: Ifefc9ec95a2c143778fbb9022d3de4f8f9628a7c

---
+++ build/build_ubuntu.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+make clean
+make distclean
+autoreconf -vif ..
+
+STAGEDIR=$PWD/stage
+
+../configure \
+     LDFLAGS=-ldl \
+     LIBS=-ldl \
+     CFLAGS="-Wall -Werror -Wno-unused-but-set-variable -Wno-pointer-sign -Wno-implicit-function-declaration -Wno-address -Wno-enum-compare" \
+    --prefix=/opt/vmware \
+    --libdir=/opt/vmware/lib64 \
+    --with-vmevent=$STAGEDIR/opt/vmware \
+    --with-vmdir=$STAGEDIR/opt/vmware \
+    --with-vmdns=$STAGEDIR/opt/vmware \
+    --with-afd=$STAGEDIR/opt/vmware \
+    --with-vmca=$STAGEDIR/opt/vmware \
+    --with-likewise=/opt/likewise \
+    --with-ssl=/usr \
+    --with-jansson=/usr \
+    --with-java=/usr/lib/jvm/java-1.8.0-openjdk-amd64 \
+    --with-maven=/usr/share/maven \
+    --with-ant=/usr/share/ant \
+    --with-python=/usr \
+    --with-config=./config \
+    --with-version="1.3.0" \
+    --enable-lightwave-build=yes \
+    ac_cv_header_vmevent=yes \
+    ac_cv_header_vmdirclient_h=yes \
+    ac_cv_lib_vmdirclient_VmDirSetupHostInstance=yes \
+    ac_cv_lib_vmdirclient_VmDirConnectionOpen=yes \
+    ac_cv_header_vmdns_h=yes \
+    ac_cv_lib_vmdnsclient_VmDnsOpenServerA=yes \
+    ac_cv_header_vmafdclient_h=yes \
+    ac_cv_lib_vmafdclient_VmAfdGetDomainNameA=yes \
+    ac_cv_header_vmca_h=yes \
+    ac_cv_lib_vmcaclient_VMCACreateSelfSignedCertificateA=yes
+    #--prefix=/opt/vmware \
+    #--libdir=/opt/vmware/lib64 \
+    #--with-vmevent=$STAGEDIR/opt/vmware \
+    #--with-vmdir=$STAGEDIR/opt/vmware \
+    #--with-vmdns=$STAGEDIR/opt/vmware \
+    #--with-afd=$STAGEDIR/opt/vmware \
+    #--with-likewise=/opt/likewise \
+    #--with-ssl=/usr \
+    #--with-jansson=/usr \
+    #--with-java=/usr/lib/jvm/java-1.8.0-openjdk-amd64 \
+    #--with-ant=/usr/share/ant \
+    #--with-python=/usr \
+    #--with-config=./config \
+    #--with-version="1.3.0" \
+    #--enable-lightwave-build=yes
