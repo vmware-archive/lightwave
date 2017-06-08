92b058e8d336d649a9634d57eb18d663317721cd
Jonathan Brown <brownj@vmware.com>
2017-06-07 17:08:45 -0700

    fix top-level photon OS build
    This change fixes the top-level photon OS build.  The directory
    vmidentity/ssoclients is now built prior to lwraft and vmdir projects
    to satisfy library dependencies when REST is enabled.
    
    Change-Id: I3214901cc1e20596cd539de775ad89f3bf96c58b

--- build/build_photon.sh
+++ build/build_photon.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
-#autoreconf -vif ..
+
+autoreconf -vif ..
 
 STAGEDIR=$PWD/stage
 
 ../configure \
     CFLAGS="-Wall -Werror -Wno-unused-but-set-variable -Wno-pointer-sign -Wno-implicit-function-declaration -Wno-address -Wno-enum-compare" \
     STAGEDIR=$STAGEDIR \
+    --disable-dependency-tracking \
     --prefix=/opt/vmware \
     --libdir=/opt/vmware/lib64 \
     --localstatedir=/var/lib/vmware \
@@ -15,14 +17,14 @@ STAGEDIR=$PWD/stage
     --with-afd=$STAGEDIR/opt/vmware \
     --with-vmca=$STAGEDIR/opt/vmware \
     --with-sts=$STAGEDIR/opt/vmware \
+    --with-oidc=$STAGEDIR/opt/vmware \
     --with-likewise=/opt/likewise \
     --with-logdir=/var/log/lightwave \
     --with-ssl=/usr \
     --with-sqlite=/usr \
     --with-jansson=/usr \
     --with-copenapi=/usr \
-    --with-oidc=/opt/vmware \
-    --with-trident=/opt/vmware \
+    --with-c-rest-engine=/usr \
     --with-java=/var/opt/OpenJDK-1.8.0.112-bin \
     --with-maven=/var/opt/apache-maven-3.3.9 \
     --with-ant=/var/opt/apache-ant-1.9.6 \
@@ -39,6 +41,16 @@ STAGEDIR=$PWD/stage
     ac_cv_header_vmdirclient_h=yes \
     ac_cv_lib_vmdirclient_VmDirSetupHostInstance=yes \
     ac_cv_lib_vmdirclient_VmDirConnectionOpen=yes \
+    ac_cv_header_jansson_h=yes \
+    ac_cv_lib_jansson_json_object=yes \
+    ac_cv_header_copenapi_copenapi_h=yes \
+    ac_cv_lib_copenapi_coapi_load_from_file=yes \
+    ac_cv_header_vmrest_h=yes \
+    ac_cv_lib_restengine_VmRESTInit=yes \
+    ac_cv_header_oidc_h=yes \
+    ac_cv_header_oidc_types_h=yes \
+    ac_cv_lib_ssooidc_OidcAccessTokenBuild=yes \
+    ac_cv_header_common_types_h=yes \
     ac_cv_header_vmdns_h=yes \
     ac_cv_lib_vmdnsclient_VmDnsOpenServerA=yes \
     ac_cv_header_vmafdclient_h=yes \
