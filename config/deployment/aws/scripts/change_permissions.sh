8ae5d201c2f1a33accd221b414dfbf4a2fb8eb3c
Amit Bapat <bapata@vmware.com>
2017-09-29 13:40:35 -0700

    Removing Code Deploy Scripts from LW repo
    Deleting aws code deploy scripts as project is being moved to another
    repo.
    
    Change-Id: I484ff8b404ce253abd9a906893c74edcec81dd62

--- config/deployment/aws/scripts/change_permissions.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Skipping changing permissions"
-echo `pwd`
--- config/deployment/aws/scripts/install_dependencies.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Add tdnf upgrade/install commands here to upgrade/install new dependencies"
-# required to create local yum repository
-tdnf install -y sed
-tdnf install -y createrepo
-
-tdnf install -y zip unzip
