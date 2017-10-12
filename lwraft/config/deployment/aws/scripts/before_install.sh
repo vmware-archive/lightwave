bf98bdf308e88a53bf8599f395cd275d6c322285
DhanashreeA <adhikari.dhanashree@gmail.com>
2017-10-12 19:20:11 +0000

    Bug Number: 1868231 lwraft: valgrind memory leak report
    This change is to resolve the memory leak in the 2.1.26
    version of cyrus-sasl library's SRP plugin.
    This version being installed is provided by photon OS
    team which has a patch for the leak.
    
    Tests done:
    Installed on local instance and tested SRP bind.
    
    Change-Id: I4040a4b21cd4bd34fd05196747242d3ae3587e61

--- lwraft/config/deployment/aws/scripts/before_install.sh
+++ lwraft/config/deployment/aws/scripts/before_install.sh
@@ -4,3 +4,7 @@ echo "Step 1: Upgrade/install createrepo and its dependencies"
 
 tdnf makecache
 tdnf install -y sed zip unzip createrepo c-rest-engine-1.0.4-2.ph1
+
+echo "Install patched version of cyrus-sasl"
+
+tdnf install -y cyrus-sasl-2.1.26-9.ph1
