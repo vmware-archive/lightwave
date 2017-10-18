vmware-archive/lightwave

commit c04d47612cafb2aa505900aa13204610a415f1be

Do Not Forward DNS Request If No Forwarders
Before, if there are no forwarders configured, VMDNS would still try to
call the forward request function which would result in an error.

This error would make response times slow, and it would also result in
an improper response packet.  Fuzz tests failed if there were no
forwardrs configured and we tried to test VMDNS.

To fix this, we call the ForwardRequest function if there is an error
generated from attempting to fulfill the request locally from VMDNS
zones.  If ForwardRequest cannot fulfill the request (for example, if
there are no forwarders configured), then it will immediately notify the
sockinterface function processing the TCP/UDP request.  If there is an
error from processing the request locally, or if the ForwardRequest
function generated an error, we will send the response from our local
request handling.

Changes:

  - sockinterface.c calls ForwardRequest if there is an error generated
from local processing, if ForwardRequest generates an error or if local
processing generates an error, then we send the response generated from
local processing.

Testing:

  - Run VMDNS with no forwarders configured
  - Run the fuzzer which went through this issue
  - Response was correctly returned if manually attempting to resolve
non-existant entry

Change-Id: Icdfa2e66812e224b3edf5c4cea643979806ea3e1

vmdns/server/common/sockinterface.c
@@ -515,7 +515,7 @@ cleanup:
 
     if (dwError != ERROR_IO_PENDING && pIoBuffer)
     {
-	    VMDNS_LOG_IO_RELEASE(pIoBuffer);
+        VMDNS_LOG_IO_RELEASE(pIoBuffer);
         VmDnsSockReleaseIoBuffer(pIoBuffer);
     }
 
@@ -569,7 +569,7 @@ VmDnsOnDisconnect(
 
     if (pIoBuffer)
     {
-	    VMDNS_LOG_IO_RELEASE(pIoBuffer);
+        VMDNS_LOG_IO_RELEASE(pIoBuffer);
         VmDnsSockReleaseIoBuffer(pIoBuffer);
     }
 
@@ -597,7 +597,7 @@ VmDnsOnDataAvailable(
 cleanup:
     if (pIoBuffer)
     {
-	    VMDNS_LOG_IO_RELEASE(pIoBuffer);
+        VMDNS_LOG_IO_RELEASE(pIoBuffer);
         VmDnsSockReleaseIoBuffer(pIoBuffer);
     }
 
@@ -641,7 +641,7 @@ VmDnsReceiveData(
 cleanup:
     if (pIoBuffer)
     {
-	    VMDNS_LOG_IO_RELEASE(pIoBuffer);
+        VMDNS_LOG_IO_RELEASE(pIoBuffer);
         VmDnsSockReleaseIoBuffer(pIoBuffer);
     }
 
@@ -682,7 +682,7 @@ VmDnsTcpReceiveData(
 cleanup:
     if (pIoBuffer)
     {
-	    VMDNS_LOG_IO_RELEASE(pIoBuffer);
+        VMDNS_LOG_IO_RELEASE(pIoBuffer);
         VmDnsSockReleaseIoBuffer(pIoBuffer);
     }
 
@@ -721,7 +721,7 @@ VmDnsUdpReceiveData(
 cleanup:
     if (pIoBuffer)
     {
-	    VMDNS_LOG_IO_RELEASE(pIoBuffer);
+        VMDNS_LOG_IO_RELEASE(pIoBuffer);
         VmDnsSockReleaseIoBuffer(pIoBuffer);
     }
 
@@ -771,7 +771,7 @@ cleanup:
 
     if (pIoBuffer)
     {
-	    VMDNS_LOG_IO_RELEASE(pIoBuffer);
+        VMDNS_LOG_IO_RELEASE(pIoBuffer);
         VmDnsSockReleaseIoBuffer(pIoBuffer);
     }
 
@@ -823,7 +823,7 @@ cleanup:
 
     if (pIoBuffer)
     {
-	    VMDNS_LOG_IO_RELEASE(pIoBuffer);
+        VMDNS_LOG_IO_RELEASE(pIoBuffer);
         VmDnsSockReleaseIoBuffer(pIoBuffer);
     }
 
@@ -893,7 +893,7 @@ cleanup:
 
     if (pIoNewBuffer)
     {
-	    VMDNS_LOG_IO_RELEASE(pIoBuffer);
+        VMDNS_LOG_IO_RELEASE(pIoBuffer);
         VmDnsSockReleaseIoBuffer(pIoNewBuffer);
     }
 
@@ -911,6 +911,7 @@ VmDnsOnTcpRequestDataRead(
     )
 {
     DWORD dwError = 0;
+    DWORD dwFrwdError = 0;
     PVM_SOCK_IO_BUFFER  pIoNewBuffer = NULL;
     PVM_SOCK_IO_BUFFER  pIoSizeBuffer = NULL;
     PBYTE pResponse = NULL;
@@ -959,7 +960,15 @@ VmDnsOnTcpRequestDataRead(
                             );
         BAIL_ON_VMDNS_ERROR(dwError);
 
-        if (!rCode)
+        if (rCode)
+        {
+            dwFrwdError = VmDnsOnForwarderRequest(
+                                    FALSE,
+                                    pSocket,
+                                    pIoBuffer
+                                    );
+        }
+        else if (!rCode || dwFrwdError)
         {
             dwError = VmDnsSockAllocateIoBuffer(
                                 VM_SOCK_EVENT_TYPE_TCP_RESPONSE_SIZE_WRITE,
@@ -1026,22 +1035,17 @@ VmDnsOnTcpRequestDataRead(
                 BAIL_ON_VMDNS_ERROR(dwError);
             }
         }
-        else
-        {
-            dwError = VmDnsOnForwarderRequest(FALSE, pSocket, pIoBuffer);
-            BAIL_ON_VMDNS_ERROR(dwError);
-        }
     }
 cleanup:
 
     if (pIoSizeBuffer)
     {
-	    VMDNS_LOG_IO_RELEASE(pIoSizeBuffer);
+        VMDNS_LOG_IO_RELEASE(pIoSizeBuffer);
         VmDnsSockReleaseIoBuffer(pIoSizeBuffer);
     }
     if (pIoNewBuffer)
     {
-	    VMDNS_LOG_IO_RELEASE(pIoNewBuffer);
+        VMDNS_LOG_IO_RELEASE(pIoNewBuffer);
         VmDnsSockReleaseIoBuffer(pIoNewBuffer);
     }
 
@@ -1060,6 +1064,7 @@ VmDnsOnUdpRequestDataRead(
     )
 {
     DWORD dwError = 0;
+    DWORD dwFrwdError = 0;
     PVM_SOCK_IO_BUFFER  pIoNewBuffer = NULL;
     PBYTE pResponse = NULL;
     DWORD dwDnsResponseSize = 0;
@@ -1085,9 +1090,16 @@ VmDnsOnUdpRequestDataRead(
                         );
     BAIL_ON_VMDNS_ERROR(dwError);
 
-    if (!rCode)
+    if (rCode)
     {
-
+        dwError = VmDnsOnForwarderRequest(
+                            TRUE,
+                            pSocket,
+                            pIoBuffer
+                            );
+    }
+    else if (!rCode || dwFrwdError)
+    {
         dwError = VmDnsSockAllocateIoBuffer(
                             VM_SOCK_EVENT_TYPE_UDP_RESPONSE_DATA_WRITE,
                             NULL,
@@ -1111,7 +1123,6 @@ VmDnsOnUdpRequestDataRead(
                         pIoBuffer->addrLen,
                         pIoNewBuffer
                         );
-
         if (dwError == ERROR_SUCCESS)
         {
             dwError = VmDnsOnUdpResponseDataWrite(
@@ -1126,21 +1137,12 @@ VmDnsOnUdpRequestDataRead(
             BAIL_ON_VMDNS_ERROR(dwError);
         }
     }
-    else
-    {
-       dwError = VmDnsOnForwarderRequest(
-                                    TRUE,
-                                    pSocket,
-                                    pIoBuffer
-                                    );
-       BAIL_ON_VMDNS_ERROR(dwError);
-    }
 
 cleanup:
 
     if (pIoNewBuffer)
     {
-	    VMDNS_LOG_IO_RELEASE(pIoBuffer);
+        VMDNS_LOG_IO_RELEASE(pIoBuffer);
         VmDnsSockReleaseIoBuffer(pIoNewBuffer);
     }
 
@@ -1309,7 +1311,7 @@ cleanup:
     return dwError;
 
 error:
-    
+
     goto cleanup;
 }
 
@@ -1534,12 +1536,12 @@ cleanup:
     }
     if (pIoNewBuffer)
     {
-	VMDNS_LOG_IO_RELEASE(pIoNewBuffer);
+        VMDNS_LOG_IO_RELEASE(pIoNewBuffer);
         VmDnsSockReleaseIoBuffer(pIoNewBuffer);
     }
     if (pIoSizeBuffer)
     {
-	VMDNS_LOG_IO_RELEASE(pIoSizeBuffer);
+        VMDNS_LOG_IO_RELEASE(pIoSizeBuffer);
         VmDnsSockReleaseIoBuffer(pIoSizeBuffer);
     }
 

commit 2f4b80c480adced0c4094224dd7c200d94a2a6e5

Lets VMDNS Send Proper Error Response
There was a regression introduced in VMDNS with c04d4761 where if local
processing of a request failed, then we would immediately bail out of
the sockinterface function for UDP/TCP.  This would prevent VMDNS from
sending proper error responses if it cannot resolve the request.

Also, there were some logical errors in processing error codes to send
the appropriate response from the sockinterface layer.

To address this, we remove the bail line after ProcessRequest and allow
the sockinterface functions to try forwarding or sending error
responses.  Also, we fix the logic when processing error codes from our
local ProcessRequest function and the Forwarder function.

Testing:

  - VMDNS with no forwarders configured
    + Try to resolve a known domain name and expect an answer saying
error
      - Got an answer saying error
    + Try to resolve a domain name in the default zone and expect an
answer with success
      - Got an answer with success

Change-Id: Id9dbc48465013a8d05ee51daac8664b7f07ee0c3

vmdns/server/common/sockinterface.c
@@ -958,7 +958,6 @@ VmDnsOnTcpRequestDataRead(
                             &dwDnsResponseSize,
                             &rCode
                             );
-        BAIL_ON_VMDNS_ERROR(dwError);
 
         if (rCode)
         {
@@ -967,8 +966,14 @@ VmDnsOnTcpRequestDataRead(
                                     pSocket,
                                     pIoBuffer
                                     );
+            if (dwFrwdError == ERROR_SUCCESS)
+            {
+                // Forwarder state machine will fulfill the response, cleanup
+                goto cleanup;
+            }
         }
-        else if (!rCode || dwFrwdError)
+
+        if (pResponse && dwDnsResponseSize > 0)
         {
             dwError = VmDnsSockAllocateIoBuffer(
                                 VM_SOCK_EVENT_TYPE_TCP_RESPONSE_SIZE_WRITE,
@@ -1036,6 +1041,7 @@ VmDnsOnTcpRequestDataRead(
             }
         }
     }
+
 cleanup:
 
     if (pIoSizeBuffer)
@@ -1088,17 +1094,22 @@ VmDnsOnUdpRequestDataRead(
                         &dwDnsResponseSize,
                         &rCode
                         );
-    BAIL_ON_VMDNS_ERROR(dwError);
-
     if (rCode)
     {
-        dwError = VmDnsOnForwarderRequest(
+        dwFrwdError = VmDnsOnForwarderRequest(
                             TRUE,
                             pSocket,
                             pIoBuffer
                             );
+        if (dwFrwdError == ERROR_SUCCESS)
+        {
+            // Forwarder state machine will fulfill the response, cleanup
+            goto cleanup;
+        }
     }
-    else if (!rCode || dwFrwdError)
+
+
+    if (pResponse && dwDnsResponseSize > 0)
     {
         dwError = VmDnsSockAllocateIoBuffer(
                             VM_SOCK_EVENT_TYPE_UDP_RESPONSE_DATA_WRITE,