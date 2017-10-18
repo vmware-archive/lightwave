1593c4a88df8c9d0fda4f398e82da4841eb8a717
Kyoung Won Kwon <kkwon@vmware.com>
2017-10-18 18:58:42 +0000

    post: deployment: enable DNS caching (PR 1980895)
    Change-Id: I5dce377d6370738804e140487ea3302f5c838d95

--- lwraft/config/deployment/aws/scripts/before_install.sh
+++ lwraft/config/deployment/aws/scripts/before_install.sh
@@ -8,3 +8,8 @@ tdnf install -y sed zip unzip createrepo c-rest-engine-1.0.4-2.ph1
 echo "Install patched version of cyrus-sasl"
 
 tdnf install -y cyrus-sasl-2.1.26-9.ph1
+
+
+echo "Step 2: Enable DNS caching"
+
+sed -i 's/hosts: files/hosts:files resolve/g' /etc/nsswitch.conf
