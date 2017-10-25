47c76686d0b7aa70e05b40c4ab5d6fb09c7db6ce
Sriram Nambakam <snambakam@vmware.com>
2017-10-25 15:32:38 -0700

    Build Lightwave Server Docker Image This is useful for development purposes at this time. Remove unused docker files
    Change-Id: Ia2772d55f2aba592a570c0d6ac6f357876db08ff

--- support/scripts/clean.sh
+++ support/scripts/clean.sh
@@ -8,6 +8,7 @@ cd $PROJECT_ROOT/build && \
 /bin/rm -rf `find $PROJECT_ROOT -name Makefile.in`
 
 /bin/rm -rf config \
+            docker \
             include \
             lwraft \
             rpmbuild \
---
+++ support/scripts/prep-container-build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+PROJECT_ROOT=$(pwd)
+
+DOCKER_ROOT=$PROJECT_ROOT/build/docker
+
+mkdir -p $DOCKER_ROOT
+
+rm -rf $DOCKER_ROOT/*
+
+cp $PROJECT_ROOT/support/docker/lightwave-init $DOCKER_ROOT
+
+cp -r $PROJECT_ROOT/build/rpmbuild/RPMS/x86_64 $DOCKER_ROOT
+
+cp $PROJECT_ROOT/support/docker/Dockerfile $DOCKER_ROOT
+
+# modify Dockerfile to use local lightwave yum repository
+
+tmpfile=$(mktemp /tmp/lw.XXXXXX)
+cat >$tmpfile <<EOF
+COPY x86_64 /tmp/vmware/lightwave/x86_64
+RUN tdnf makecache && \
+    tdnf install -y sed createrepo && \
+    sed -i -e "s/https:\/\/dl.bintray.com/file:\/\/\/tmp/" -e "s/gpgcheck=1/gpgcheck=0/" /etc/yum.repos.d/lightwave.repo && \
+    createrepo /tmp/vmware/lightwave
+EOF
+sed -i -e "/# Build hook/r $tmpfile" -e "//d" $DOCKER_ROOT/Dockerfile
+rm $tmpfile
+
