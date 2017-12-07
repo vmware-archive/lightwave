19fcb24589fca6b3852ba874c94d11964d2385df
Harish Udaiya Kumar <hudaiyakumar@vmware.com>
2017-12-07 19:47:11 +0000

    Build webui war file using maven plugin and include in RPM
    Change-Id: I8aad75270d8c12776d3816060c36be2cdc884d76

---
+++ vmidentity/webui/src/app/app.module.ts
@@ -0,0 +1,53 @@
+/*
+ *  Copyright (c) 2012-2017 VMware, Inc.  All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *  use this file except in compliance with the License.  You may obtain a copy
+ *  of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, without
+ *  warranties or conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the
+ *  License for the specific language governing permissions and limitations
+ *  under the License.
+ */
+
+import { NgModule, Renderer2, EventEmitter, Output }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClarityModule } from 'clarity-angular';
+import { AppComponent }  from './app.component';
+import { HttpModule, JsonpModule }    from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from './auth.service';
+import { LoginComponent }  from './login.component';
+import { LazyLoadedLevel1Component } from './vmdir.component';
+import { SdeditorComponent } from './sdeditor.component';
+import { ObjectAddComponent } from './objectadd.component';
+import { VmdirService }  from './vmdir.service';
+import { IdentitySourceService }  from './identitysources.service';
+import { ConfigService }  from './config.service';
+import { UtilsService }  from './utils.service';
+import { SDDLParser }  from './sddlparser';
+import { VmdirUtils }  from './vmdir.utils';
+import { VmdirSchemaService }  from './vmdirschema.service';
+import { AppRoutingModule, routableComponents } from './app.routing.module';
+ import { DateTimePickerModule } from 'ng-pick-datetime';
+@NgModule({
+  imports:      [
+                 AppRoutingModule,
+                 ClarityModule,
+                 BrowserModule,
+                 FormsModule,
+                 BrowserAnimationsModule,
+                 HttpModule,
+                 HttpClientModule,
+                 JsonpModule,
+                 DateTimePickerModule
+                ],
+  declarations: [ AppComponent, routableComponents, LazyLoadedLevel1Component, SdeditorComponent, ObjectAddComponent],
+  providers:    [ AuthService, VmdirService, VmdirUtils, VmdirSchemaService, ConfigService, UtilsService, IdentitySourceService, SDDLParser ],
+  bootstrap:    [ AppComponent ],
+})
+export class AppModule { }
