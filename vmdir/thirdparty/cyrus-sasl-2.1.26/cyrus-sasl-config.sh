a0c40a60bb59b9334e935fe437a00edae34eea4e
Adam Bernstein <abernstein@vmware.com>
2018-01-08 21:37:15 +0000

    Modified cyrus-sasl-2.1.26 for winjoin project
    Modified to build the gss-spngo plugin. Disabled
    Force use of libgssspnego.so plugin when "GSS-SPENGEO"
    protocol is required by the client. Needed to disable
    "SSF negotiation" while authenticating with Windows clients.
    
    Compile using ./cyrus-sasl-config.sh && make
    ./cyrus-sasl-config.sh --force-debug for debug library.
    Original code: ftp://ftp.cyrusimap.org/cyrus-sasl/cyrus-sasl-2.1.26.tar.gz
    
    Change-Id: I147f92f84636672dfd61c5f67975eceecf5e0525

---
+++ vmdir/thirdparty/cyrus-sasl-2.1.26/cyrus-sasl-config.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+usage()
+{
+  if [ -n "$1" ]; then
+    echo "$1"
+  fi
+  echo "usage: $0 [--force-debug]"
+  exit 1
+}
+
+while [ `echo "$1" | grep -c '^-'` -gt 0 ]; do
+  if [ \( " $1" = " --help" \) -o \( " $1" = " -h" \) ]; then
+    usage ""
+  elif [ " $1" = " --force-debug" ]; then
+    CDEBUGFLAGS='-g -O0 -fPIC'
+    shift
+  else
+    usage "ERROR: unknown option $1"
+  fi
+done
+
+autoreconf -i -f \
+  -I./cmulocal \
+  -I./config \
+  -I./saslauthd/config
+
+
+CFLAGS=$CDEBUGFLAGS  ./configure --prefix=/ \
+    --bindir=/usr/bin \
+    --libdir=/usr/lib \
+    --sysconfdir=/etc \
+    --with-plugindir=/usr/lib/sasl2 \
+    --without-dblib \
+    --with-saslauthd=/run/saslauthd \
+    --without-authdaemond \
+    --disable-macos-framework \
+    --disable-sample \
+    --disable-digest \
+    --disable-otp \
+    --disable-plain \
+    --disable-anon \
+    --enable-srp \
+    --enable-gss_mutexes \
+    --disable-static \
+    --enable-shared \
+    --enable-fast-install \
+    --enable-krb4
+
+cp /usr/bin/libtool .
