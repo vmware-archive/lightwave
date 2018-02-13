9e6f604758fec27e8d00b86ba1f20c93d8908151
sruo <sruo@vmware.com>
2018-02-13 02:31:07 +0000

    PR 2050323: disable expensive search log due to memory leak in superlog
    Change-Id: I7bf3920ffb8a3beb854e0a529fe2e551faf81559

--- lwraft/server/rest-head/ldapapi.c
+++ lwraft/server/rest-head/ldapapi.c
@@ -141,8 +141,10 @@ VmDirRESTLdapSearch(
     dwError = VmDirMLSearch(pSearchOp);
     BAIL_ON_VMDIR_ERROR(dwError);
 
-    dwError = VmDirLogSearchParameters(pSearchOp);
-    BAIL_ON_VMDIR_ERROR(dwError);
+// BUGBUG, this causes memory leak in VmdirDeleteConnection -> _VmDirScrubSuperLogContent
+// need to fix up super logging code before reenable this.
+//    dwError = VmDirLogSearchParameters(pSearchOp);
+//    BAIL_ON_VMDIR_ERROR(dwError);
 
     // set operation result
     dwError = VmDirRESTEncodeEntryArray(
