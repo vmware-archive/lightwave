1d717fb42d90121341b744486ec2dcf6f9c79a64
DhanashreeA <adhikari.dhanashree@gmail.com>
2018-03-14 19:25:07 +0000

    [pre-prod] POST to pick up new lwis version
    Bug Number: 2078089
    
    Change-Id: Iabec7b78ebbe38cb64c8d10b4df0c9753308a03a

--- lwraft/config/deployment/aws/scripts/before_install.sh
+++ lwraft/config/deployment/aws/scripts/before_install.sh
@@ -18,13 +18,13 @@ if [ $DISTRO == "1.0" ]; then
     tdnf install -y createrepo \
     c-rest-engine-1.2-3.ph1 \
     cyrus-sasl-2.1.26-10.ph1 \
-    likewise-open-6.2.11-7 \
+    likewise-open-6.2.11-8 \
     openssl-1.0.2n
 else
     tdnf install -y createrepo_c \
     c-rest-engine-1.1-1.ph2 \
     cyrus-sasl-2.1.26 \
-    likewise-open-6.2.11 \
+    likewise-open-6.2.11-8.lwph2 \
     openssl-1.0.2n
 fi
 
