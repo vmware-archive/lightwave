74751dbeaa0efdf818552a4b7a2f81c4b9dea702
Sriram Nambakam <snambakam@vmware.com>
2018-04-30 10:22:38 -0700

    Support multiple photon versions
    Change-Id: I924a767c8748bc5d1c531a9d6c5b70770bc28e60
    
    Separate builds and runtime on Photon-1.0 versus Photon 2.0
    
    Jira ID: CNA-802
    
    Change-Id: I558433d932e108a899ebe90fc0c4184c3ed557be

--- support/scripts/prep-container-build.sh
+++ support/scripts/prep-container-build.sh
@@ -1,9 +1,65 @@
 #!/bin/bash
 
+
+inject_docker_file_photon_1()
+{
+    #
+    # Assumes that sed and createrepo are already installed
+    #
+    tmpfile=$(mktemp /tmp/lw.XXXXXX)
+    cat >$tmpfile <<-EOF
+	COPY x86_64 /tmp/vmware/lightwave/x86_64
+	RUN sed -i -e "s/https:\/\/dl.bintray.com/file:\/\/\/tmp/" -e "s/gpgcheck=1/gpgcheck=0/" /etc/yum.repos.d/lightwave.repo && \
+	createrepo /tmp/vmware/lightwave && \
+	tdnf makecache
+	EOF
+    sed -i -e "/# Build hook/r $tmpfile" -e "//d" $DOCKER_ROOT/Dockerfile
+    rm $tmpfile
+}
+
+inject_docker_file_photon_2()
+{
+    #
+    # Assumes that sed and createrepo are already installed
+    #
+    tmpfile=$(mktemp /tmp/lw.XXXXXX)
+
+    cat >$tmpfile <<-EOF
+	COPY x86_64 /tmp/vmware/lightwave/x86_64
+	COPY lightwave.repo /etc/yum.repos.d
+	RUN createrepo /tmp/vmware/lightwave && \
+	tdnf makecache
+	EOF
+
+    sed -i -e "/# Build hook/r $tmpfile" -e "//d" $DOCKER_ROOT/Dockerfile
+    rm $tmpfile
+}
+
+#
+# Main
+#
+
 PROJECT_ROOT=$(pwd)
 
 DOCKER_ROOT=$PROJECT_ROOT/build/docker
-DOCKER_SRC_ROOT=$PROJECT_ROOT/support/docker/sts
+
+OSVER=
+if [ $# -gt 0 ]; then
+    OSVER=$1
+fi
+
+case "$OSVER" in
+    photon1)
+        DOCKER_SRC_ROOT=$PROJECT_ROOT/support/docker/sts/photon1
+        ;;
+    photon2)
+        DOCKER_SRC_ROOT=$PROJECT_ROOT/support/docker/sts/photon2
+        ;;
+    *)
+        echo "Error: Unsupported o/s version: $OSVER"
+        exit 1
+        ;;
+esac
 
 mkdir -p $DOCKER_ROOT
 
@@ -12,20 +68,20 @@ rm -rf $DOCKER_ROOT/*
 cp -r $PROJECT_ROOT/build/rpmbuild/RPMS/x86_64 $DOCKER_ROOT
 
 cp $DOCKER_SRC_ROOT/lightwave-init $DOCKER_ROOT
+LIGHTWAVE_REPO_FILE=$DOCKER_SRC_ROOT/lightwave.repo
+if [ -f $LIGHTWAVE_REPO_FILE ]; then
+    cp $LIGHTWAVE_REPO_FILE $DOCKER_ROOT
+fi
 cp $DOCKER_SRC_ROOT/Dockerfile $DOCKER_ROOT
 
 # modify Dockerfile to use local lightwave yum repository
 
-#
-# Assumes that sed and createrepo are already installed
-#
-tmpfile=$(mktemp /tmp/lw.XXXXXX)
-cat >$tmpfile <<EOF
-COPY x86_64 /tmp/vmware/lightwave/x86_64
-RUN sed -i -e "s/https:\/\/dl.bintray.com/file:\/\/\/tmp/" -e "s/gpgcheck=1/gpgcheck=0/" /etc/yum.repos.d/lightwave.repo && \
-    createrepo /tmp/vmware/lightwave && \
-    tdnf makecache
-EOF
-sed -i -e "/# Build hook/r $tmpfile" -e "//d" $DOCKER_ROOT/Dockerfile
-rm $tmpfile
+case "$OSVER" in
+    photon1)
+        inject_docker_file_photon_1
+        ;;
+    photon2)
+        inject_docker_file_photon_2
+        ;;
+esac
 
