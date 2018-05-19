2db64619fb12c7d1ea6c94224c79e4cf5313805c
Kyoung Won Kwon <kkwon@vmware.com>
2018-05-19 18:44:42 +0000

    Emergency check in to correct likewise-open version in the POST deployment script
    Change-Id: I2357ca5faa2ba228e436c0bb5acca436f9a85005

--- lwraft/config/deployment/aws/scripts/before_install.sh
+++ lwraft/config/deployment/aws/scripts/before_install.sh
@@ -18,13 +18,13 @@ if [ $DISTRO == "1.0" ]; then
     tdnf install -y createrepo \
     c-rest-engine-1.2-5.ph1 \
     cyrus-sasl-2.1.26-10.ph1 \
-    likewise-open-6.2.11-9 \
+    likewise-open-6.2.11-8 \
     openssl-1.0.2n
 else
     tdnf install -y createrepo_c \
     c-rest-engine-1.2-5.ph2 \
     cyrus-sasl-2.1.26 \
-    likewise-open-6.2.11-9.lwph2 \
+    likewise-open-6.2.11-8.lwph2 \
     openssl-1.0.2n
 fi
 
