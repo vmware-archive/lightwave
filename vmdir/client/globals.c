e9a28ce85117eb08bf103db78526263bad9f9dbd
Pranav Vaidya <pranavv@vmware.com>
2018-06-13 16:51:11 +0000

    Fixes for VmDirGetServersDetailed API
    Change-Id: I4554801eb868d77d88103d9ca752ddb5e94ad90e

--- vmdir/client/globals.c
+++ vmdir/client/globals.c
@@ -39,5 +39,5 @@ VMDIR_ERROR_CODE_DESC VMDIR_RPC_ERROR_Table[] =
                                  VMDIR_RPC_ERROR_TABLE_INITIALIZER;
 DWORD VMDIR_RPC_ERROR_Table_size = sizeof(VMDIR_RPC_ERROR_Table)/sizeof(VMDIR_RPC_ERROR_Table[0]);
 
-PCSTR PVMDIR_SERVER_INFO_ATTRS[3] = {"createTimeStamp", "cn", NULL};
+PCSTR PVMDIR_SERVER_INFO_ATTRS[] = {"createTimeStamp", "cn", NULL};
 
