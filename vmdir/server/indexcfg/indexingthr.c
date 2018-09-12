2c4bd12e69c5c51885df105f8b3c6ea46976b2b2
sruo <sruo@vmware.com>
2018-09-12 23:25:54 +0000

    PR 2197927: address various memory issues
    Change-Id: I50299975b250ecb26b2bd36caf7bb90ce7a30265

--- vmdir/server/indexcfg/indexingthr.c
+++ vmdir/server/indexcfg/indexingthr.c
@@ -88,6 +88,7 @@ resume:
 
             // compute new task
             VmDirFreeIndexingTask(pTask);
+            pTask = NULL;
             dwError = VmDirIndexingTaskCompute(&pTask);
             BAIL_ON_VMDIR_ERROR(dwError);
         }
