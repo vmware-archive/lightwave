35ac5e66d6753d0a77dbfd919e3a34a83b2f61d9
Divya Thaluru <dthaluru@vmware.com>
2018-11-01 14:02:34 -0700

    MutentCA: Updated mutentca hmake script to populate ca and db config values
    Change-Id: I0874706b16f9703593a65e14be3ece1f0be82d75

--- support/tests/lightwave/mutentca/setup.sh
+++ support/tests/lightwave/mutentca/setup.sh
@@ -10,6 +10,16 @@ if [ -z "$AWS_ACCESS_KEY_ID" -o -z "$AWS_SECRET_ACCESS_KEY" ]; then
   echo "Missing AWS_ACCESS_KEY_ID or AWS_SECRET_ACCESS_KEY in .env file. ca security plugin tests will fail."
 fi
 
+#warn on missing ca name for ca
+if [ -z "$CA_NAME" ]; then
+  echo "Missing CA_NAME in .env file. CA will be initialized with default name."
+fi
+
+#warn on missing ca org for ca
+if [ -z "$CA_ORG" ]; then
+  echo "Missing CA_ORG in .env file. CA org will be initialized with default value."
+fi
+
 #prepare by installing rpms built in this build
 rpm -Uvh --nodeps buildrpms/x86_64/lightwave-client*.rpm
 rpm -Uvh --nodeps buildrpms/x86_64/lightwave-post*.rpm
@@ -45,6 +55,26 @@ sed -i 's|@@ARN@@|$AWS_KMS_ARN|' /opt/vmware/share/config/casecurity-aws-kms.jso
 #using allow all config for rest api integration tests
 cp scripts/mutentca-policy-allowall.json /opt/vmware/share/config/mutentca-policy-allowall.json
 sed -i 's/mutentca-policy.json/mutentca-policy-allowall.json/g' /opt/vmware/share/config/mutentcaconfig.json
+#
+
+#modify ca post config
+sed -i 's|@@LWSERVER@@|$primary|' /opt/vmware/share/config/mutentca-post.json
+sed -i 's|@@POSTSERVER@@|localhost|' /opt/vmware/share/config/mutentca-post.json
+sed -i 's|@@LWDOMAIN@@|$LIGHTWAVE_DOMAIN|' /opt/vmware/share/config/mutentca-post.json
+#
+
+#modify ca config
+if [ -z "$CA_NAME" ]; then
+  sed -i 's|@@CANAME@@|LightwaveCA|' /opt/vmware/share/config/mutentcaconfig.json
+else
+  sed -i 's|@@CANAME@@|$CA_NAME|' /opt/vmware/share/config/mutentcaconfig.json
+fi
+
+if [ -z "$CA_ORG" ]; then
+  sed -i 's|@@CAORG@@|VMware Inc|' /opt/vmware/share/config/mutentcaconfig.json
+else
+  sed -i 's|@@CAORG@@|$CA_ORG|' /opt/vmware/share/config/mutentcaconfig.json
+fi
 #
 
 #restart post and mutentca
