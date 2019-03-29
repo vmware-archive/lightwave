e00ff1015d84dd1fccf1ac8b88a9cafa6cc53769
Sung Ruo <sruo@vmware.com>
2019-03-29 18:04:41 +0000

    Add password policy test skeleton into integartion test
    place holder for password policy tests
    
    Change-Id: I3f989deda2ecad79ea988018524e30d2881a135a

---
+++ vmdir/testing/integration_tests/ppolicy/main.c
@@ -0,0 +1,50 @@
+/*
+ * Copyright ©2019 VMware, Inc.  All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the “License”); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an “AS IS” BASIS, without
+ * warranties or conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include "includes.h"
+
+DWORD
+TestSetup(
+    PVMDIR_TEST_STATE pState
+    )
+{
+    return 0;
+}
+
+
+DWORD
+TestCleanup(
+    PVMDIR_TEST_STATE pState
+    )
+{
+    return 0;
+}
+
+DWORD
+TestRunner(
+    PVMDIR_TEST_STATE pState
+    )
+{
+    DWORD dwError = 0;
+
+    printf("Testing password policy code ...\n");
+    BAIL_ON_VMDIR_ERROR(dwError);
+
+    printf("Password policy tests finished successfully.\n");
+
+cleanup:
+    return dwError;
+error:
+    goto cleanup;
+}
